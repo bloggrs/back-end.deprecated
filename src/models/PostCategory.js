d2b3eac06f9029ed91f31f0153c280c80f1139c5
Gjergj Kadriu <gjergjk71@gmail.com>
2022-01-02 15:59:47 +0100

    push

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,24 +1,22 @@
-"use strict"
+"use strict";
 
 const { ErrorHandler } = require("../utils/error");
 
 module.exports = (sequelize, DataTypes) => {
-    let options = { 
-        defaultScope: {
-        },
-        scopes: {
-        }
-    }
-    let PostCategory = sequelize.define('PostCategory', {
+  let options = {
+    defaultScope: {},
+    scopes: {},
+  };
+  let PostCategory = sequelize.define("PostCategory", {}, options);
+  PostCategory.findByPkOrError = async (pk) => {
+    let postCategory = await PostCategory.findByPk(pk);
+    if (!postCategory) throw new ErrorHandler.get404("PostCategory");
+    return postCategory;
+  };
+  PostCategory.associate = (models) => {
+    PostCategory.belongsTo(models.Post);
+    PostCategory.belongsTo(models.Category);
+  };
 
-    }, options);
-    PostCategory.findByPkOrError = async pk => {
-        let postCategory = await PostCategory.findByPk(pk)
-        if (!postCategory) throw new ErrorHandler.get404("PostCategory")
-        return postCategory;
-    }
-    PostCategory.associate = models => {
-    }
-    
-    return PostCategory;
+  return PostCategory;
 };
